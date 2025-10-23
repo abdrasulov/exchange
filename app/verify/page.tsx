b0dd439574ce76725cc4741b7cad64b10335a7ff
abdrasulov <abdrasulov@gmail.com>
2025-10-23 12:41:30 +0600

    Add user verification via sumsub

--- app/verify/page.tsx
+++ app/verify/page.tsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import {createAccessToken, createSignature} from "@/lib/sumsub";
+import SumsubWidget from "@/app/verify/sumsubWidget";
 
 axios.interceptors.request.use(createSignature, function (error) {
     return Promise.reject(error);
@@ -19,20 +20,9 @@ export default async function Verify() {
         error = err.response?.data?.message || "Failed to fetch token";
     }
 
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 p-6">
-            <script src="https://static.sumsub.com/idensic/static/sns-websdk-builder.js"></script>
-            <div id="sumsub-websdk-container"></div>
+    if (!token) {
+        return <p className="text-center text-red-500 mt-20">Failed to load verification, {error}</p>;
+    }
 
-            {error && <p className="text-red-500 mb-4">{error}</p>}
-            {token ? (
-                <div className="bg-white p-6 rounded-2xl shadow-lg">
-                    <h2 className="text-xl font-bold mb-2 text-gray-800">Access Token</h2>
-                    <p className="font-mono break-all text-gray-700">{token}</p>
-                </div>
-            ) : (
-                <p className="text-gray-600">No token available</p>
-            )}
-        </div>
-    );
+    return <SumsubWidget accessToken={token} />;
 }

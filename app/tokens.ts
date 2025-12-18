74b7a9cc5e8257302cb6fd745d8fde55805aa7e5
abdrasulov <abdrasulov@gmail.com>
2025-12-18 16:39:18 +0600

    Add usdc

--- app/tokens.ts
+++ app/tokens.ts
@@ -9,7 +9,14 @@ export function getSupportedTokens(addressFormat: string) : Token[] {
                 name: "Tether USD",
                 contractAddress: "0xdAC17F958D2ee523a2206206994597C13D831ec7",
                 decimals: 6,
-            }
+            },
+            {
+                id: "usdc",
+                symbol: "USDC",
+                name: "USDC",
+                contractAddress: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
+                decimals: 6,
+            },
         ]
     }
 

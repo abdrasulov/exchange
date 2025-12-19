9b7c58ed749ab594e6c96bfcb9915411eecea4b1
abdrasulov <abdrasulov@gmail.com>
2025-12-19 17:50:33 +0600

    Display action buttons

--- components/AssetCard.tsx
+++ components/AssetCard.tsx
@@ -39,49 +39,49 @@ const AssetCard = ({
           <div className="text-xs text-neutral-500 dark:text-neutral-400">{fiatAmount}</div>
         </div>
       </div>
-      {/*<div className="bg-white px-4 py-6 dark:bg-neutral-900 rounded-b-xl border-t border-neutral-200*/}
-      {/*              dark:border-neutral-800">*/}
-      {/*  <div className="grid grid-cols-3 gap-4">*/}
-      {/*    <button type="submit" className="flex flex-col gap-2 border border-neutral-200 hover:bg-neutral-100*/}
-      {/*                  dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 items-center justify-center*/}
-      {/*                  rounded-lg bg-neutral-50 py-3 text-sm font-medium text-neutral-900 dark:bg-neutral-800">*/}
-      {/*      <div className="h-8 w-8 items-center justify-center rounded-full bg-neutral-200 dark:bg-neutral-700*/}
-      {/*                    flex">*/}
-      {/*        <svg className="h-4 w-4 rotate-45" fill="none" viewBox="0 0 24 24" stroke="currentColor"*/}
-      {/*             id="Windframe_JklDlkXF4">*/}
-      {/*          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"*/}
-      {/*                d="M5 10l7-7m0 0l7 7m-7-7v18"></path>*/}
-      {/*        </svg>*/}
-      {/*      </div>*/}
-      {/*      Send*/}
-      {/*    </button>*/}
-      {/*    <button type="submit" className="flex flex-col gap-2 border border-neutral-200 hover:bg-neutral-100*/}
-      {/*                  dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 items-center justify-center*/}
-      {/*                  rounded-lg bg-neutral-50 py-3 text-sm font-medium text-neutral-900 dark:bg-neutral-800">*/}
-      {/*      <div className="h-8 w-8 items-center justify-center rounded-full bg-neutral-200 dark:bg-neutral-700*/}
-      {/*                    flex">*/}
-      {/*        <svg className="h-4 w-4 rotate-45" fill="none" viewBox="0 0 24 24" stroke="currentColor"*/}
-      {/*             id="Windframe_R5sSGf0hF">*/}
-      {/*          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"*/}
-      {/*                d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>*/}
-      {/*        </svg>*/}
-      {/*      </div>*/}
-      {/*      Receive*/}
-      {/*    </button>*/}
-      {/*    <button type="submit" className="flex flex-col gap-2 border border-neutral-200 hover:bg-neutral-100*/}
-      {/*                  dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 items-center justify-center*/}
-      {/*                  rounded-lg bg-neutral-50 py-3 text-sm font-medium text-neutral-900 dark:bg-neutral-800">*/}
-      {/*      <div className="h-8 w-8 items-center justify-center rounded-full bg-neutral-200 dark:bg-neutral-700*/}
-      {/*                    flex">*/}
-      {/*        <svg className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" id="Windframe_Vxi5xDhtK">*/}
-      {/*          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"*/}
-      {/*                d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4"></path>*/}
-      {/*        </svg>*/}
-      {/*      </div>*/}
-      {/*      Swap*/}
-      {/*    </button>*/}
-      {/*  </div>*/}
-      {/*</div>*/}
+      <div className="bg-white px-4 py-6 dark:bg-neutral-900 rounded-b-xl border-t border-neutral-200
+                    dark:border-neutral-800">
+        <div className="grid grid-cols-3 gap-4">
+          <button type="submit" className="flex flex-col gap-2 border border-neutral-200 hover:bg-neutral-100
+                        dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 items-center justify-center
+                        rounded-lg bg-neutral-50 py-3 text-sm font-medium text-neutral-900 dark:bg-neutral-800">
+            <div className="h-8 w-8 items-center justify-center rounded-full bg-neutral-200 dark:bg-neutral-700
+                          flex">
+              <svg className="h-4 w-4 rotate-45" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                   id="Windframe_JklDlkXF4">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                      d="M5 10l7-7m0 0l7 7m-7-7v18"></path>
+              </svg>
+            </div>
+            Send
+          </button>
+          <button type="submit" className="flex flex-col gap-2 border border-neutral-200 hover:bg-neutral-100
+                        dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 items-center justify-center
+                        rounded-lg bg-neutral-50 py-3 text-sm font-medium text-neutral-900 dark:bg-neutral-800">
+            <div className="h-8 w-8 items-center justify-center rounded-full bg-neutral-200 dark:bg-neutral-700
+                          flex">
+              <svg className="h-4 w-4 rotate-45" fill="none" viewBox="0 0 24 24" stroke="currentColor"
+                   id="Windframe_R5sSGf0hF">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                      d="M19 14l-7 7m0 0l-7-7m7 7V3"></path>
+              </svg>
+            </div>
+            Receive
+          </button>
+          <button type="submit" className="flex flex-col gap-2 border border-neutral-200 hover:bg-neutral-100
+                        dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 items-center justify-center
+                        rounded-lg bg-neutral-50 py-3 text-sm font-medium text-neutral-900 dark:bg-neutral-800">
+            <div className="h-8 w-8 items-center justify-center rounded-full bg-neutral-200 dark:bg-neutral-700
+                          flex">
+              <svg className="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" id="Windframe_Vxi5xDhtK">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                      d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4"></path>
+              </svg>
+            </div>
+            Swap
+          </button>
+        </div>
+      </div>
     </div>
   )
 
